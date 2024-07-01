a0388f6dd85396adffaabb982ba6f1a91263cf62
RashtriyaGaurav <rashtriya.73@gmail.com>
2024-07-01 13:11:44 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -7,17 +7,26 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+let count = 0; // Variable to track connected users
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', (socket) => {
     console.log('New client connected');
-    
+
+    // Increase count and emit to all clients
+    count++;
+    io.emit('userCount', count);
+
     socket.on('message', (message) => {
         io.emit('message', message);
     });
 
     socket.on('disconnect', () => {
         console.log('Client disconnected');
+        // Decrease count and emit to all clients
+        count--;
+        io.emit('userCount', count);
     });
 });
 
